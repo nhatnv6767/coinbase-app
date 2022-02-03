07017baf92ee40afa14aa98968720915774c0eab
nhatnguyen6767 <nhatnvse90183@gmail.com>
2022-02-03 23:38:32 +0900

    connect wallet button

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,9 @@ import { useWeb3 } from '@3rdweb/hooks'
 
 export default function Home() {
   const { address, connectWallet } = useWeb3()
-  return <div></div>
+  return (
+    <div>
+      <button onClick={()=> connectWallet('injected')}>Connect Wallet</button>
+    </div>
+  )
 }
