9b27e53416f36c769af321df786734d315998e5e
nhatnguyen6767 <nhatnvse90183@gmail.com>
2022-02-03 23:47:36 +0900

    style of wallet connect

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,30 @@ import { useWeb3 } from '@3rdweb/hooks'
 export default function Home() {
   const { address, connectWallet } = useWeb3()
   return (
-    <div>
-      <button onClick={()=> connectWallet('injected')}>Connect Wallet</button>
-    </div>
+    <Wrapper>
+      <WalletConnect>
+       <button onClick={()=> connectWallet('injected')}>
+         Connect Wallet
+       </button>
+      </WalletConnect>
+    </Wrapper>
   )
 }
+
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  max-width: 100vw;
+  background-color: #0a0b0d;
+  color: white;
+  display: grid;
+  place-items: center;
+`
+
+const WalletConnect = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
