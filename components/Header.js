1ebfd3558fcc775252ec90dc5210d21e43f3f906
nhatnguyen6767 <nhatnvse90183@gmail.com>
2022-02-05 11:29:29 +0900

    button style

--- components/Header.js
+++ components/Header.js
@@ -27,3 +27,16 @@ const Title = styled.div`
 const ButtonsContainer = styled.div`
     display: flex;
 `
+
+const Button = styled.div`
+    border: 1px solid #282b2f;
+    padding: 0.8rem;
+    font-size: 1.3rem;
+    font-weight: 500;
+    border-radius: 0.4rem;
+    margin-right: 1rem;
+
+    &:hover {
+        cursor: pointer;
+    }
+`
