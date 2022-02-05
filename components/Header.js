29418bec3c6d37ff1459d663cfa976efb46ec2c5
nhatnguyen6767 <nhatnvse90183@gmail.com>
2022-02-05 11:46:52 +0900

    Title

--- components/Header.js
+++ components/Header.js
@@ -4,11 +4,14 @@ import styled from 'styled-components'
 const Header = () => {
   return (
     <Wrapper>
+      <Title>Assets</Title>
+      <ButtonsContainer>
         <Button style={{ backgroundColor: '#3773f5', color: '#000000'} }>
             Buy / Sell
         </Button>
 
         <Button>Send / Receive</Button>
+      </ButtonsContainer>
     </Wrapper>
   )
 }
