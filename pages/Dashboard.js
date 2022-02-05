6963edf67d8b6c7c20be88251dd4a5a37bb37c27
nhatnguyen6767 <nhatnvse90183@gmail.com>
2022-02-05 11:52:27 +0900

    dashboard style

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -6,3 +6,17 @@ const Dashboard = ({address}) => {
 }
 
 export default Dashboard
+
+
+const Wrapper = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    background-color: #0a0b0d;
+    color: white;
+
+`
+
+const MainContainer = styled.div`
+    flex: 1;
+`
