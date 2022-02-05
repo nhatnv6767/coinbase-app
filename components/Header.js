b9c75b66f0709c83c43ff4159770550166f04a99
nhatnguyen6767 <nhatnvse90183@gmail.com>
2022-02-05 11:24:59 +0900

    style

--- components/Header.js
+++ components/Header.js
@@ -9,3 +9,17 @@ const Header = () => {
 }
 
 export default Header
+
+const Wrapper = styled.div`
+    width: calc(100% - 3rem);
+    padding: 1rem 1.5rem;
+    border-bottom: 1px solid #282b2f;
+    display: flex;
+    align-items: center;
+`
+
+const Title = styled.div`
+    font-size: 2rem;
+    font-weight: 600;
+    flex: 1;
+`
