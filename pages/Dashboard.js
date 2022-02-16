08434f790745a5550881cdf0b9e727137e4df604
nhatnguyen6767 <nhatnvse90183@gmail.com>
2022-02-16 21:59:12 +0900

    the next day

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
+import styled from 'styled-components'
 
 const Dashboard = ({address}) => {
-  return <Header/>
+  return (
+    <Wrapper>
+      <Header/>
+    </Wrapper>
+  )
 }
 
 export default Dashboard
