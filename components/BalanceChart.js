62a0a1a7a14de73f793fee85f23333a219013520
nhatnguyen6767 <nhatnvse90183@gmail.com>
2022-02-21 23:38:48 +0900

    Add chart and balance

---
+++ components/BalanceChart.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import { Line } from 'react-chartjs-2'
+
+
+const BalanceChart = () => {
+  return (
+    <Wrapper></Wrapper>
+  )
+}
+
+export default BalanceChart
+
+const Wrapper = styled.div``
